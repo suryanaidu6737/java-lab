9e9785f2aa22869becbadf37247d439f6f799e8b
suryanaidu6737 <yerramsettisurya445@gmail.com>
2025-10-15 12:27:26 +0530

    Create TimeDemo.java

---
+++ TimeDemo.java
@@ -0,0 +1,28 @@
+import java.time.*;
+import java.time.format.*;
+import java.time.temporal.*;
+class TimeDemo
+{
+	public static void main(String[] args)
+	{
+		LocalDate date=LocalDate.now();
+		System.out.println(date);
+		LocalDateTime datetime=LocalDateTime.now();
+		System.out.println(datetime);
+		DateTimeFormatter formtime = DateTimeFormatter.ofPattern("dd/MM/yyyy,hh:mm:ss");
+		System.out.println(datetime.format(formtime));
+		LocalDate nextmonday=date.with(TemporalAdjusters.next(DayOfWeek.MONDAY));
+		System.out.println(nextmonday);
+		LocalDate previousmonday=date.with(TemporalAdjusters.previous(DayOfWeek.MONDAY));
+		System.out.println(previousmonday);
+		LocalDate daymonth=date.with(TemporalAdjusters.firstDayOfNextMonth());
+		System.out.println(daymonth);
+		DayOfWeek first=daymonth.getDayOfWeek();
+		System.out.println(first);
+		LocalDate lastdaymonth=date.with(TemporalAdjusters.lastDayOfMonth());
+		System.out.println(lastdaymonth);
+		DayOfWeek last=lastdaymonth.getDayOfWeek();
+		System.out.println(last);
+		
+	}
+}
